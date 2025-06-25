bf15a5075f22bc07b24e274590d76cb6ccdb82d8
theBappy <abappy575@gmail.com>
2025-06-25 23:50:19 +0600

    Dark and white mode toggle enable with custom dark color

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
-import { ClerkProvider } from '@clerk/nextjs'
+import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { cn } from "@/lib/utils";
 
 const font = Open_Sans({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
-
 
 export const metadata: Metadata = {
   title: "Discord@theBappy",
@@ -21,13 +22,17 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body
-        className={`${font.className} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+      <html lang="en" suppressHydrationWarning>
+        <body className={cn(font.className, 'bg-white dark:bg-[#313338] antialiased'
+        )}>
+          <ThemeProvider
+          attribute='class'
+          defaultTheme="dark"
+          enableSystem={false}
+          storageKey="discord-theme"
+          >{children}</ThemeProvider>
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
