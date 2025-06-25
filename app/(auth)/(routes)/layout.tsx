551f7aa0a49c46eba757de21dba8a0edd390fb60
theBappy <abappy575@gmail.com>
2025-06-25 23:10:45 +0600

    Initial structure of the project with auth and main organization route structure

---
+++ app/(auth)/(routes)/layout.tsx
@@ -0,0 +1,11 @@
+
+
+const layout = ({children}: {children: React.ReactNode}) => {
+  return (
+    <div className="bg-red-500 h-screen">
+        {children}
+    </div>
+  )
+}
+
+export default layout
